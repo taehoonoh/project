4e5b859996793df22c5634a860583462dddb57ce
taehoonoh <dkrakcjstk14@gmail.com>
2020-07-27 14:37:25 +0900

    Update index.js

--- aboutime/js/index.js
+++ aboutime/js/index.js
@@ -6,18 +6,18 @@ console.log(playbt);
 
 var body = document.querySelector('body');
 
-
+var iframe = document.querySelector('iframe');
 
 menu[0].addEventListener('click',function(){
 
 popup.classList.remove('none');
-
+iframe.src= null;
 })
 
 playbt.addEventListener('click',function(){
 
     popup.classList.add('none');
-
+    iframe.src= "https://www.youtube.com/embed/7OIFdWk83no?rel=0&autoplay=1&start=16 ";
 })
 
     var loading = document.querySelector('.loading')
